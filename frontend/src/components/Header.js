9d2834a1944bebfe913414db6048d903aed4d2f5
Certurix <certurix@outlook.fr>
2024-11-15 11:57:05 +0100

    Ajout header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="py-6 px-8">
+      <h1 className="text-2xl font-bold">Bonjour, Marc</h1>
+      <p className="text-gray-500 mt-1">Consultez vos dernières informations et détails</p>
+    </div>
+  );
+}
+
+export default Header;
