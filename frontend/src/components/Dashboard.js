9157bbe1c755b7bd67375b9ea179aba24b75f588
Certurix <46199479+Certurix@users.noreply.github.com>
2024-11-26 12:24:07 +0100

    Ajout du routing pour le dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Header from './Header';
 import Calendar from './Dashboard/SmallCalendar';
@@ -7,25 +8,35 @@ import EventStats from './Dashboard/EventStats';
 import TimeDistribution from './Dashboard/Charts/Time';
 import EventAdd from './EventAdd';
 
+import Plannings from '../pages/Plannings';
+
 const Dashboard = () => {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      <Sidebar />
-      <div className="flex-1 p-6">
-        <Header />
-        <div className="grid grid-cols-3 gap-6">
-          <div className="col-span-2 space-y-6">
-            <Calendar />
-            <EventsList />
-          </div>
-          <div className="space-y-6">
-            <EventStats />
-            <TimeDistribution />
-          </div>
+    <Router>
+      <div className="flex min-h-screen bg-gray-100">
+        <Sidebar />
+        <div className="flex-1 p-6">
+          <Header />
+          <Routes>
+            <Route path="/" element={
+              <div className="grid grid-cols-3 gap-6">
+                <div className="col-span-2 space-y-6">
+                  <Calendar />
+                  <EventsList />
+                </div>
+                <div className="space-y-6">
+                  <EventStats />
+                  <TimeDistribution />
+                </div>
+              </div>
+            } />
+            <Route path="/plannings" element={<Plannings/>} />
+            <Route path="/class" element={<div>Ma classe</div>} />
+          </Routes>
+          <EventAdd />
         </div>
-        <EventAdd/>
       </div>
-    </div>
+    </Router>
   );
 }
 
