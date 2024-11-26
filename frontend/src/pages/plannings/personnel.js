11527c93b5712351cd45fcc353c08e3ec2df94eb
Certurix <certurix@outlook.fr>
2024-11-26 18:58:59 +0100

    Introduction d'ajout du planning perso

--- frontend/src/pages/plannings/personnel.js
+++ frontend/src/pages/plannings/personnel.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
 
 const PlanningPerso = () => {
-  return <div></div>;
+  const [events, setEvents] = useState([
+    {
+      title: 'RDV',
+      start: new Date(),
+      end: new Date(moment().add(1, 'hours').toDate()),
+    },
+    {
+      title: 'Atelier cuisine',
+      start: new Date(moment().add(2, 'hours').toDate()),
+      end: new Date(moment().add(3, 'hours').toDate()),
+    },
+  ]);
+
+  const handleSelectSlot = ({ start, end }) => {
+    const title = window.prompt('New Event Name');
+    if (title) {
+      setEvents([...events, { start, end, title }]);
+    }
+  };
+
+  const handleSelectEvent = (event) => {
+    
+  };
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        selectable
+        onSelectSlot={handleSelectSlot}
+        onSelectEvent={handleSelectEvent}
+        defaultView="week"
+        views={['month', 'week', 'day', 'agenda']}
+        style={{ height: 500 }}
+      />
+    </div>
+  );
 };
 
 export default PlanningPerso;
