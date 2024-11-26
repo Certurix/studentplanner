19b53205f4c70feeeae9c86f6b8fa1d655e737db
Certurix <46199479+Certurix@users.noreply.github.com>
2024-11-26 22:56:28 +0100

    MAJ taille et propriétés formulaire d'ajout évenement

--- frontend/src/components/Modals/ModalEventAdd.js
+++ frontend/src/components/Modals/ModalEventAdd.js
@@ -1,23 +1,13 @@
 // frontend/src/components/Modals/ModalEventAdd.js
 
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import EventAdd from '../EventAdd';
-import { css } from '@emotion/react';
-
-const customModalStyles = css`
-  .modal-content {
-    background-color: rgba(255, 0, 255, 0.1); /* Adjust the opacity as needed */
-  }
-  .modal-backdrop {
-    background-color: rgba(0, 0, 0, 0.5); /* Adjust the opacity as needed */
-  }
-`;
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import EventAdd from "../Dashboard/EventAdd";
 
 const ModalEventAdd = ({ show, handleClose }) => {
   return (
-    <Modal show={show} onHide={handleClose} dialogClassName={customModalStyles}>
+    <Modal show={show} size={"lg"} onHide={handleClose} fullscreen={false}>
       <Modal.Body>
         <EventAdd />
       </Modal.Body>
