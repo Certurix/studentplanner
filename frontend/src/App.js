5b6e70c0f2b1481574ee9e5a42a2e200df6c1b8f
Certurix <certurix@outlook.fr>
2024-11-26 18:57:37 +0100

    Ajout du router principal dans le dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Dashboard />
-    </div>
+    </Router>
   );
 }
 
