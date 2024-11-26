42080fa4413b2bf5dfe0fac63082f2de5cdd3116
Certurix <46199479+Certurix@users.noreply.github.com>
2024-11-26 22:55:16 +0100

    Ajout de couleurs globales

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,17 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['"Inter"', ...defaultTheme.fontFamily.sans],
+        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#6C6FD7",
+        primary_hover: "#3F43CA"
       },
     },
   },
   plugins: [],
-}
+};
