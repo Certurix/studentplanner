f62a15dfc232209386507f702ca6ddcaa934beda
Certurix <certurix@outlook.fr>
2025-01-28 12:07:23 +0100

    Modif répartition par type de plannings

--- frontend/src/components/Dashboard/Charts/Time.js
+++ frontend/src/components/Dashboard/Charts/Time.js
@@ -1,16 +1,13 @@
 const TimeDistribution = () => {
   const data = [
-    { label: "Cours", value: 40, color: "bg-orange-500" },
-    { label: "Travail / Job étudiant", value: 28, color: "bg-yellow-500" },
-    { label: "Interrogations / Contrôles", value: 12, color: "bg-blue-500" },
-    { label: "Rendez-vous", value: 3, color: "bg-red-500" },
-    { label: "Sorties", value: 72, color: "bg-green-500" },
-    { label: "Autres", value: 45, color: "bg-gray-500" },
+    { label: "Personnel", value: 40, color: "bg-orange-500" },
+    { label: "Professionnel", value: 28, color: "bg-yellow-500" },
+    { label: "Scolaire", value: 12, color: "bg-blue-500" },
   ];
 
   return (
     <div className="p-6 bg-white rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold mb-4">RÉPARTITION HEURES</h2>
+      <h2 className="text-xl font-semibold mb-4">RÉPARTITION PLANNINGS</h2>
       {data.map((item, index) => (
         <div key={index} className="mb-4">
           <p className="mb-1">{item.label}</p>
