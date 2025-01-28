8571eff1ad06b90e42315fa9e7a4091dfcd49f47
Certurix <certurix@outlook.fr>
2025-01-28 12:06:34 +0100

    Déplacement du répertoire des composants d'évenements

--- frontend/src/components/Modals/ModalEventAdd.js
+++ frontend/src/components/Modals/ModalEventAdd.js
@@ -3,7 +3,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import EventAdd from "../Dashboard/EventAdd";
+import EventAdd from "../Dashboard/Events/EventAdd";
 
 const ModalEventAdd = ({ show, handleClose }) => {
   return (
