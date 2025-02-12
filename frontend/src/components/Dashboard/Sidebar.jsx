e1f218b9b02145a8b5a4cc176e46bdd92cea55fb
Certurix <46199479+Certurix@users.noreply.github.com>
2025-02-12 01:25:34 +0100

    feat: Add avatar fetching functionality and display in Sidebar component

--- frontend/src/components/Dashboard/Sidebar.jsx
+++ frontend/src/components/Dashboard/Sidebar.jsx
@@ -1,17 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Icon } from "@iconify-icon/react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
-import './Sidebar.css';
+import "./Sidebar.css";
 
 export default function Sidebar({ data }) {
   const [searchQuery, setSearchQuery] = useState("");
+  const [avatar, setAvatar] = useState(null);
+  const [error, setError] = useState(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (data && data.name && data.lastname) {
+      getAvatar();
+    }
+  }, [data]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     navigate(`/events/search?query=${searchQuery}`);
   };
+
+  function getAvatar() {
+    fetch(
+      `https://cors-anywhere.herokuapp.com/https://img.buymeacoffee.com/api/?name=${data.name}+${data.lastname}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.blob();
+      })
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        setAvatar(url);
+      })
+      .catch((error) => setError(error.message));
+  }
 
   return (
     <aside
@@ -20,19 +51,29 @@ export default function Sidebar({ data }) {
     >
       <div>
         <div className="flex items-center mb-6">
-          <img
-            src={`/logo.svg`}
-            alt="Logo StudentPlanner"
-            className="mr-2"
-          />
-          <h1 className="text-2xl font-semibold whitespace-nowrap overflow-hidden text-ellipsis">StudentPlanner</h1>
+          <img src={`/logo.svg`} alt="Logo StudentPlanner" className="mr-2" />
+          <h1 className="text-2xl font-semibold whitespace-nowrap overflow-hidden text-ellipsis">
+            StudentPlanner
+          </h1>
         </div>
 
         <form className="max-w-md mx-auto" onSubmit={handleSearchSubmit}>
           <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
               </svg>
             </div>
             <input
@@ -49,18 +90,26 @@ export default function Sidebar({ data }) {
 
         <Navbar expand="lg" className="flex-column p-0">
           <Nav className="flex-column w-full">
-            <Nav.Link
-              as={NavLink}
-              to="/"
-              className="flex items-center mb-4"
-            >
-              <Icon icon="tabler:home" width="20" height="20" className="mr-2" /> Accueil
+            <Nav.Link as={NavLink} to="/" className="flex items-center mb-4">
+              <Icon
+                icon="tabler:home"
+                width="20"
+                height="20"
+                className="mr-2"
+              />{" "}
+              Accueil
             </Nav.Link>
             <NavDropdown
               title={
                 <div className="flex items-center justify-between w-full">
                   <span className="flex items-center">
-                    <Icon icon="tabler:calendar-week" width="20" height="20" className="mr-2" /> Plannings
+                    <Icon
+                      icon="tabler:calendar-week"
+                      width="20"
+                      height="20"
+                      className="mr-2"
+                    />{" "}
+                    Plannings
                   </span>
                 </div>
               }
@@ -93,7 +142,13 @@ export default function Sidebar({ data }) {
               title={
                 <div className="flex items-center justify-between w-full">
                   <span className="flex items-center">
-                    <Icon icon="tabler:school" width="20" height="20" className="mr-2" /> Ma classe
+                    <Icon
+                      icon="tabler:school"
+                      width="20"
+                      height="20"
+                      className="mr-2"
+                    />{" "}
+                    Ma classe
                   </span>
                 </div>
               }
@@ -127,25 +182,43 @@ export default function Sidebar({ data }) {
           to="/settings"
           className="flex items-center p-2 mb-4"
         >
-          <Icon icon="tabler:settings" width="20" height="20" className="mr-2" /> Paramètres
+          <Icon
+            icon="tabler:settings"
+            width="20"
+            height="20"
+            className="mr-2"
+          />{" "}
+          Paramètres
         </Nav.Link>
         <Nav.Link
           as={NavLink}
           to="/support"
           className="flex items-center p-2 mb-4"
         >
-          <Icon icon="tabler:lifebuoy" width="20" height="20" className="mr-2" /> Assistance
+          <Icon
+            icon="tabler:lifebuoy"
+            width="20"
+            height="20"
+            className="mr-2"
+          />{" "}
+          Assistance
         </Nav.Link>
         <hr />
         <div className="px-4 py-2">
           <div className="flex items-center">
-            <img
-              src="https://via.placeholder.com/40"
-              alt="profil"
-              className="rounded-full mr-4 border border-gray-300"
-            />
+            {avatar ? (
+              <img
+                src={avatar}
+                alt="profil"
+                className="w-10 h-10 rounded-full mr-4 border border-gray-300 bg-gray-200"
+              />
+            ) : (
+              <div className="w-10 h-10 rounded-full mr-4 border border-gray-300 bg-gray-200" />
+            )}
             <div>
-              <p className="font-semibold">{data.name} {data.lastname}</p>
+              <p className="font-semibold">
+                {data.name} {data.lastname}
+              </p>
               <p className="text-sm text-gray-500">{data.email}</p>
             </div>
           </div>
