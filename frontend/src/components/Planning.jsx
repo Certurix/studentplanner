6763abba47a592e05135bfe8c4ffb51389ff1935
Certurix <certurix@outlook.fr>
2025-02-25 13:30:02 +0100

    refactor: Plannings pages are now a single Plannings component

---
+++ frontend/src/components/Planning.jsx
@@ -0,0 +1,162 @@
+import React, { useState, useRef } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import CustomToolbar from "./CustomToolbar";
+import { Button, Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaCompress, FaExpand } from "react-icons/fa";
+import "./Dashboard/Calendar.css";
+
+moment.locale("fr");
+const localizer = momentLocalizer(moment);
+
+const messages = {
+  allDay: "Toute la journée",
+  previous: "Précédent",
+  next: "Suivant",
+  today: "Aujourd'hui",
+  month: "Mois",
+  week: "Semaine",
+  day: "Jour",
+  agenda: "Agenda",
+  date: "Date",
+  time: "Heure",
+  event: "Événement",
+  noEventsInRange: "Aucun événement dans cette période.",
+  showMore: (total) => `+ ${total} plus`,
+};
+
+const formats = {
+  timeGutterFormat: (date, culture, localizer) =>
+    localizer.format(date, "HH:mm", culture),
+  eventTimeRangeFormat: ({ start, end }, culture, localizer) =>
+    `${localizer.format(start, "HH:mm", culture)} - ${localizer.format(
+      end,
+      "HH:mm",
+      culture
+    )}`,
+  agendaTimeRangeFormat: ({ start, end }, culture, localizer) =>
+    `${localizer.format(start, "HH:mm", culture)} - ${localizer.format(
+      end,
+      "HH:mm",
+      culture
+    )}`,
+};
+
+const Planning = ({ title, initialEvents }) => {
+  const [events, setEvents] = useState(initialEvents);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const calendarRef = useRef(null);
+
+  const handleSelectSlot = ({ start, end }) => {
+    const eventTitle = window.prompt("New Event Name");
+    if (eventTitle) {
+      setEvents([...events, { start, end, title: eventTitle }]);
+    }
+  };
+
+  const handleSelectEvent = (event) => {
+    alert(event.title);
+  };
+
+  const toggleFullscreen = () => {
+    setIsFullscreen(!isFullscreen);
+  };
+
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md">
+      <div className="calendar-container">
+        <Calendar
+          localizer={localizer}
+          culture="fr"
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          selectable
+          onSelectSlot={handleSelectSlot}
+          onSelectEvent={handleSelectEvent}
+          defaultView="week"
+          views={["month", "week", "day", "agenda"]}
+          style={{ height: "31.25rem" }}
+          className="bg-white rounded-lg shadow-sm no-border"
+          components={{
+            toolbar: (props) => (
+              <CustomToolbar
+                {...props}
+                onNavigate={props.onNavigate}
+                onView={props.onView}
+                view={props.view}
+              />
+            ),
+          }}
+          messages={messages}
+          formats={formats}
+          ref={calendarRef}
+        />
+      </div>
+      <div className="flex justify-end mt-3">
+        <Button
+          variant="primary"
+          onClick={toggleFullscreen}
+          className="w-10 h-9 flex items-center justify-center"
+        >
+          {isFullscreen ? <FaCompress /> : <FaExpand />}
+        </Button>
+      </div>
+      <Modal
+        show={isFullscreen}
+        onHide={toggleFullscreen}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        fullscreen
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="calendar-container">
+            <Calendar
+              localizer={localizer}
+              events={events}
+              startAccessor="start"
+              endAccessor="end"
+              selectable
+              onSelectSlot={handleSelectSlot}
+              onSelectEvent={handleSelectEvent}
+              defaultView="week"
+              views={["month", "week", "day", "agenda"]}
+              style={{ height: "100vh" }}
+              className="bg-white rounded-lg shadow-sm no-border"
+              components={{
+                toolbar: (props) => (
+                  <CustomToolbar
+                    {...props}
+                    onNavigate={props.onNavigate}
+                    onView={props.onView}
+                    view={props.view}
+                  />
+                ),
+              }}
+              messages={messages}
+              formats={formats}
+              ref={calendarRef}
+            />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={toggleFullscreen}
+            className="w-10 h-10 flex items-center justify-center"
+          >
+            <FaCompress />
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default Planning;
